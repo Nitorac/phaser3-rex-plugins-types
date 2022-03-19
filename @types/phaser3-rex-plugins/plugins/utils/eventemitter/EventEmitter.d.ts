ca76925049304e3626302512a686bf301a2df865
Nitorac <nitorac.r@gmail.com>
2022-03-19 02:18:13 +0100

    Fixed TS 2507 appearing in ESM module

--- @types/phaser3-rex-plugins/plugins/utils/eventemitter/EventEmitter.d.ts
+++ @types/phaser3-rex-plugins/plugins/utils/eventemitter/EventEmitter.d.ts
@@ -1,5 +1,5 @@
 declare module 'phaser3-rex-plugins/plugins/utils/eventemitter/EventEmitter' {
-    import * as EE from 'eventemitter3';
+    import { EventEmitter as EE } from 'eventemitter3';
 
     export default class EventEmitter<EventTypes extends string | symbol = string | symbol> extends EE<EventTypes> {
         shutdown(): void;
